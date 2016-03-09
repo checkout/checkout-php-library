8e613fa8f6bde6972b31e0c9b1c574c1e24d0150
nicolas-maalouf-cko <nicolas.maalouf@checkout.com>
2016-03-09 16:01:01 +0000

    Added PaymentPlan shared model. Fixed response parsing.

--- com/checkout/ApiServices/RecurringPayments/ResponseModels/RecurringPayment.php
+++ com/checkout/ApiServices/RecurringPayments/ResponseModels/RecurringPayment.php
@@ -13,14 +13,7 @@ class RecurringPayment extends \com\checkout\ApiServices\SharedModels\BaseHttp
 {
 	protected $_object;
 	protected $_id;
-	protected $_name;
-	protected $_planTrackId;
-	protected $_autoCapTime;
-	protected $_currency;
-	protected $_value;
-	protected $_cycle;
-	protected $_recurringCount;
-	protected $_status;
+	protected $_paymentPlans;
 	protected $_totalCollectionCount;
 	protected $_totalCollectionValue;
 	
@@ -31,17 +24,12 @@ class RecurringPayment extends \com\checkout\ApiServices\SharedModels\BaseHttp
         parent::__construct($response);
 
         $this->_setObject ( $response->getObject() );
-        $this->_setId ( $response->getId() );
-        $this->_setName ( $response->getName() );
-        $this->_setPlanTrackId ( $response->getPlanTrackId() );
-        $this->_setAutoCapTime ( $response->getAutoCapTime() );
-        $this->_setCurrency ( $response->getCurrency() );
-        $this->_setValue ( $response->getValue() );
-        $this->_setCycle ( $response->getCycle() );
-        $this->_setRecurringCount ( $response->getRecurringCount() );
-        $this->_setStatus ( $response->getStatus() );
         $this->_setTotalCollectionCount ( $response->getTotalCollectionCount() );
         $this->_setTotalCollectionValue ( $response->getTotalCollectionValue() );
+
+        if($response->getPaymentPlans()) {
+			$this->_setPaymentPlans ( $response->getPaymentPlans () );
+		}
 	}
 
 
@@ -51,87 +39,6 @@ class RecurringPayment extends \com\checkout\ApiServices\SharedModels\BaseHttp
 	public function getObject ()
 	{
 		return $this->_object;
-	}
-
-
-	/**
-	 * @return mixed
-	 */
-	public function getId ()
-	{
-		return $this->_id;
-	}
-
-
-	/**
-	 * @return mixed
-	 */
-	public function getName ()
-	{
-		return $this->_name;
-	}
-
-
-	/**
-	 * @return mixed
-	 */
-	public function getPlanTrackId ()
-	{
-		return $this->_planTrackId;
-	}
-
-
-	/**
-	 * @return mixed
-	 */
-	public function getAutoCapTime ()
-	{
-		return $this->_autoCapTime;
-	}
-
-
-	/**
-	 * @return mixed
-	 */
-	public function getCurrency ()
-	{
-		return $this->_currency;
-	}
-
-	
-	/**
-	 * @return mixed
-	 */
-	public function getValue ()
-	{
-		return $this->_value;
-	}
-
-
-	/**
-	 * @return mixed
-	 */
-	public function getCycle ()
-	{
-		return $this->_cycle;
-	}
-
-
-	/**
-	 * @return mixed
-	 */
-	public function getRecurringCount ()
-	{
-		return $this->_recurringCount;
-	}
-
-
-	/**
-	 * @return mixed
-	 */
-	public function getStatus ()
-	{
-		return $this->_status;
 	}
 
 
@@ -154,6 +61,15 @@ class RecurringPayment extends \com\checkout\ApiServices\SharedModels\BaseHttp
 
 
 	/**
+	 * @return mixed
+	 */
+	public function getPaymentPlans ()
+	{
+		return $this->_paymentPlans;
+	}
+
+
+	/**
 	 * @param mixed $object
 	 */
 	private function _setObject ( $object )
@@ -163,82 +79,28 @@ class RecurringPayment extends \com\checkout\ApiServices\SharedModels\BaseHttp
 
 
 	/**
-	 * @param mixed $id
+	 * @param mixed $paymentPlans
 	 */
-	private function _setId ( $id )
+	protected function _setPaymentPlans ( $paymentPlans )
 	{
-		$this->_id = $id;
-	}
+        $paymentPlansArray = $paymentPlans->toArray();
+		$paymentPlansToReturn = array();
+		if($paymentPlansArray) {
+			foreach($paymentPlansArray as $item){
+				$paymentPlan  = new \com\checkout\ApiServices\SharedModels\PaymentPlan();
+				$paymentPlan->setPlanId($item['planId']);
+				$paymentPlan->setName($item['name']);
+				$paymentPlan->setPlanTrackId($item['planTrackId']);
+				$paymentPlan->setAutoCapTime($item['autoCapTime']);
+				$paymentPlan->setCurrency($item['currency']);
+				$paymentPlan->setValue($item['value']);
+				$paymentPlan->setRecurringCount($item['recurringCount']);
+				$paymentPlan->setStatus($item['status']);
+				$paymentPlansToReturn[] = $paymentPlan;
+			}
+		}
 
-	
-	/**
-	 * @param mixed $name
-	 */
-	private function _setName ( $name )
-	{
-		$this->_name = $name;
-	}
-
-		/**
-	 * @param mixed
-	 */
-	public function _setPlanTrackId ( $planTrackId )
-	{
-		$this->_planTrackId = $planTrackId;
-	}
-
-
-	/**
-	 * @param mixed
-	 */
-	public function _setAutoCapTime ( $autoCapTime )
-	{
-		$this->_autoCapTime = $autoCapTime;
-	}
-
-
-	/**
-	 * @param mixed
-	 */
-	public function _setCurrency ( $currency )
-	{
-		$this->_currency = $currency;
-	}
-
-	
-	/**
-	 * @param mixed
-	 */
-	public function _setValue ( $value )
-	{
-		$this->_value = $value;
-	}
-
-
-	/**
-	 * @param mixed
-	 */
-	public function _setCycle ( $cycle )
-	{
-		$this->_cycle = $cycle;
-	}
-
-
-	/**
-	 * @param mixed
-	 */
-	public function _setRecurringCount ( $recurringCount )
-	{
-		$this->_recurringCount = $recurringCount;
-	}
-
-
-	/**
-	 * @param mixed
-	 */
-	public function _setStatus ( $status )
-	{
-		$this->_status = $status;
+		$this->_paymentPlans = $paymentPlansToReturn;
 	}
 
 
